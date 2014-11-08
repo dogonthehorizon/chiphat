f7eb8fb4f70ed3be2641661cf8375dd95a5242a7
Fernando Freire <dogonthehorizon@gmail.com>
2014-11-08 08:45:00 -0800

    Updates project metadata and LICENSE.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject chiphat "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Clojure API for accessing the HipChat v2 API."
+  :url "https://github.com/dogonthehorizon/chiphat"
+  :license {:name "BSD"
+            :url "http://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
