d91b3bb5ca1bb1dfbf7c0257e7b7208707fe49e6
Fernando Freire <dogonthehorizon@gmail.com>
2014-11-09 23:58:58 -0800

    Updates clojure to 1.7 alpha and adds http-kit and cheshire deps.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/dogonthehorizon/chiphat"
   :license {:name "BSD"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0-alpha3"]
+                 [http-kit            "2.1.16"]
+                 [cheshire            "5.3.1"]])
