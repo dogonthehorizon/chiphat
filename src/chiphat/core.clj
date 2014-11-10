d4fe25bb12611c29cf71b6288b2356e6c5483448
Fernando Freire <dogonthehorizon@gmail.com>
2014-11-09 23:59:42 -0800

    Adds basic token manipulation methods.

--- src/chiphat/core.clj
+++ src/chiphat/core.clj
@@ -1,6 +1,19 @@
 (ns chiphat.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def base-url
+  "https://api.hipchat.com/v2")
+
+(def ^:dynamic *token*
+  "Required auth token for interacting with HipChat."
+  (atom nil))
+
+(defn set-token!
+  "Replace the current token with the given input."
+  [new-token]
+  (reset! *token* new-token))
+
+(defmacro with-token
+  "Evaluates body with token temporarily set to a new token."
+  [new-token & body]
+  `(binding [*token* ~new-token]
+     ~@body))
