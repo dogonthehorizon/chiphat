027795b9c54e16541ecbe31d9792d09e58aaa856
Fernando Freire <dogonthehorizon@gmail.com>
2014-11-15 14:19:12 -0800

    Adds new room methods and refactors room tests.
    This commit adds support for:
    
    * getting recent room history
    * sending notifications to rooms
    * sending replies to messages in rooms
    
    Room ns tests were also refactored to not use with-token. Future commits
    will remove this dependency completely.

--- src/chiphat/rooms.clj
+++ src/chiphat/rooms.clj
@@ -1,4 +1,5 @@
 (ns chiphat.rooms
+  (:refer-clojure :exclude [update])
   (:require [chiphat.core :refer :all]
             [chiphat.util :refer [url-encode]]))
 
@@ -9,6 +10,11 @@
   "Get all available rooms."
   [& [{:keys [start-index max-results include-archived] :as opts}]]
   (make-request :get base-endpoint {:query opts}))
+
+(defn get-one
+  "Get room details."
+  [name-or-id]
+  (make-request :get (str base-endpoint "/" (url-encode name-or-id))))
 
 (defn create
   "Create a new room."
@@ -21,7 +27,25 @@
   [name-or-id]
   (make-request :delete (str base-endpoint "/" (url-encode name-or-id))))
 
-(defn get-one
-  "Get room details."
-  [name-or-id]
-  (make-request :get (str base-endpoint "/" (url-encode name-or-id))))
+(defn recent-room-history
+  "Fetch the latest chat history for this room."
+  [name-or-id & [{:keys [max-results timezone not-before] :as opts}]]
+  (make-request :get
+                (str base-endpoint "/" (url-encode name-or-id) "/history/latest")
+                {:body opts}))
+
+(defn send-notification
+  "Send a message to a room."
+  [name-or-id message & [{:keys [color notify message_format] :as opts}]]
+  (let [body (assoc opts :message message)]
+    (make-request :post
+                  (str base-endpoint "/" (url-encode name-or-id) "/notification")
+                  {:body body})))
+
+(defn send-reply
+  "Send a reply to a room."
+  [name-or-id parent-message-id message]
+  (make-request :post
+                (str base-endpoint "/" (url-encode name-or-id) "/reply")
+                {:body {:parentMessageId parent-message-id
+                        :message message}}))
--- test/chiphat/rooms_test.clj
+++ test/chiphat/rooms_test.clj
@@ -22,30 +22,24 @@
   \"https://api.hipchat.com/v2/room/969638/statistics\"}}, \"topic\": \"\",
   \"xmpp_jid\": \"37252_foobar@conf.hipchat.com\"}")
 
+(defn- response-body
+  "Private helper function to get the json body out of a request."
+  [response]
+  (-> (get-in response [:opts :body])
+      json/parse-string))
+
 (deftest get-all-functionality
-  (testing "get-all should return all the current rooms."
-    (with-token (atom (System/getenv "CHIPHAT_API_TOKEN"))
-      (let [response (parse-response (room/get-all))]
-        (is (map? response))
-        (is (> (count (:items response)) 0))))))
-
-(deftest create-functionality
   (with-fake-http [{:url "https://api.hipchat.com/v2/room"
-                    :method :post}
-                   {:status 201
-                    :body create-response-body}]
-    (testing "create should create a new room."
-      (with-token (atom (System/getenv "CHIPHAT_API_TOKEN"))
-        (is (= 975185 (:id (parse-response (room/create "foobar")))))))))
-
-(deftest delete-functionality
-  (with-fake-http [{:url "https://api.hipchat.com/v2/room/foobar"
-                    :method :delete}
-                   {:status 200
-                    :body nil}]
-    (testing "delete should delete an existing room."
-      (with-token (atom (System/getenv "CHIPHAT_API_TOKEN"))
-        (is (= nil (parse-response (room/delete "foobar"))))))))
+                    :method :get}
+                   {:status 200}]
+    (testing "get-all should return all the current rooms."
+      (let [response @(room/get-all {:start-index 0
+                                     :max-results 1
+                                     :include-archived false})
+            query-params (get-in response [:opts :query-params])]
+        (is (= 0 (:start-index query-params)))
+        (is (= 1 (:max-results query-params)))
+        (is (not (:include-archived query-params)))))))
 
 (deftest get-one-functionality
   (with-fake-http [{:url "https://api.hipchat.com/v2/room/foobar"
@@ -53,7 +47,66 @@
                    {:status 200
                     :body get-response-body}]
     (testing "get-one should retrieve information about an existing room."
-      (with-token (atom (System/getenv "CHIPHAT_API_TOKEN"))
-        (let [response (parse-response (room/get-one "foobar"))]
-          (is (= 969638 (:id response)))
-          (is (= "foobar" (:name response))))))))
+      (let [response (parse-response (room/get-one "foobar"))]
+        (is (= 969638 (:id response)))
+        (is (= "foobar" (:name response)))))))
+
+(deftest create-functionality
+  (with-fake-http [{:url "https://api.hipchat.com/v2/room"
+                    :method :post}
+                   {:status 201
+                    :body create-response-body}]
+    (testing "create should create a new room."
+      (is (= 975185 (:id (parse-response (room/create "foobar"))))))))
+
+(deftest delete-functionality
+  (with-fake-http [{:url "https://api.hipchat.com/v2/room/foobar"
+                    :method :delete}
+                   {:status 200
+                    :body nil}]
+    (testing "delete should delete an existing room."
+      (is (= nil (parse-response (room/delete "foobar")))))))
+
+(deftest recent-room-history-functionality
+  (with-fake-http [{:url "https://api.hipchat.com/v2/room/foobar/history/latest"
+                    :method :get}
+                   {:status 200}]
+    (testing "recent-room-history should return the most recent history for an
+             existing room and respect optionally passed in parameters."
+      (let [response @(room/recent-room-history "foobar" {:max-results 1})
+            body (response-body response)
+            max-results (get body "max-results")]
+        (is (= 200 (:status response)))
+        (is (= 1 max-results))))))
+
+(deftest send-notification-functionality
+  (with-fake-http [{:url "https://api.hipchat.com/v2/room/foobar/notification"
+                    :method :post}
+                   {:status 204
+                    :body nil}]
+    (testing "send-notification should post a notification to an existing room."
+      (let [response @(room/send-notification "foobar"
+                                              "hello world"
+                                              {:color "red"
+                                               :notify true})
+            body (response-body response)
+            message (get body "message")
+            color (get body "color")
+            notify? (get body "notify")]
+        (is (= 204 (:status response)))
+        (is (= color "red"))
+        (is notify?)
+        (is (= message "hello world"))))))
+
+(deftest send-reply-functionality
+  (with-fake-http [{:url "https://api.hipchat.com/v2/room/foobar/reply"
+                    :method :post}
+                   {:status 204
+                    :body nil}]
+    (let [response @(room/send-reply "foobar"
+                                     "f0d8a8bf-d006-489a-a05e-9f76457a1c39"
+                                     "hello world!")
+          body (response-body response)
+          message (get body "message")]
+      (is (= 204 (:status response)))
+      (is (= "hello world!" message)))))
