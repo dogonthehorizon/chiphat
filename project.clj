59ead68ab2907356e2cab038a1a4681145b8dab0
Fernando Freire <dogonthehorizon@gmail.com>
2014-12-06 23:36:04 -0800

    Bumps minor version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chiphat "0.1.1"
+(defproject chiphat "0.2.0-SNAPSHOT"
   :description "Clojure library for accessing the HipChat v2 API."
   :url "https://github.com/dogonthehorizon/chiphat"
   :license {:name "BSD"
