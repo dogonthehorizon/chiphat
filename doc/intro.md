5a16cda1f93006705163ac76c77210220664cf34
Fernando Freire <dogonthehorizon@gmail.com>
2014-12-06 21:31:34 -0800

    Updates project deps and adds documentation with marginalia.

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to chiphat
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/dogonthehorizon/chiphat"
   :license {:name "BSD"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha3"]
-                 [http-kit            "2.1.18"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
+                 [http-kit            "2.1.19"]
                  [cheshire            "5.3.1"]]
+  :plugins [[lein-marginalia "0.8.0"]]
   :profiles {:dev {:dependencies [[http-kit.fake "0.2.2"]]}})
