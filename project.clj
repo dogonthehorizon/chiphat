a790a4e88f988ed276baf488129c56f3e264d6a1
Fernando Freire <dogonthehorizon@gmail.com>
2015-01-12 21:09:29 -0800

    Bumps minor version and updates Clojure version.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject chiphat "0.2.0"
+(defproject chiphat "0.3.0"
   :description "Clojure library for accessing the HipChat v2 API."
   :url "https://github.com/dogonthehorizon/chiphat"
   :license {:name "BSD"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                  [http-kit            "2.1.19"]
                  [cheshire            "5.4.0"]]
   :plugins [[lein-marginalia "0.8.0"]]
