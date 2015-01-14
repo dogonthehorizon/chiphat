e6e4c884f7448141b62f058179ddce01f180e26b
Fernando Freire <dogonthehorizon@gmail.com>
2015-01-13 18:28:38 -0800

    Adds tools.namespace for development workflow.

--- project.clj
+++ project.clj
@@ -7,4 +7,6 @@
                  [http-kit            "2.1.19"]
                  [cheshire            "5.4.0"]]
   :plugins [[lein-marginalia "0.8.0"]]
-  :profiles {:dev {:dependencies [[http-kit.fake "0.2.2"]]}})
+  :profiles {:dev {:dependencies [[http-kit.fake "0.2.2"]
+                                  [org.clojure/tools.namespace  "0.2.8"]]
+                   :source-paths ["dev/"]}})
